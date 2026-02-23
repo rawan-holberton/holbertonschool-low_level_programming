8ed4302bbc30f3ba5fad3c64844f2381b298f78b
rawan <rawansafouan@gmail.com>
2026-02-23 15:10:56 +0000

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - prints the size of various types
- *
- * Return: 0
- */
+* main - prints the size of various types
+*
+* Return: 0
+*/
 int main(void)
 {
     printf("Size of a char: %zu byte(s)\n", sizeof(char));
