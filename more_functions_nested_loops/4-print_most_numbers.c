34be69c61f6ef15112e056d7a2d2c3afc0f88989
Rawan <rawansafouan@gmail.com>
2026-02-27 09:55:22 +0000

    add tassk 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9, except 2 and 4
+ *
+ * You can only use _putchar twice in your code
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+			_putchar(c);
+	}
+	_putchar('\n');
+}
