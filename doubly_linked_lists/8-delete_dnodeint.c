b7c7e056f99fd1acd184fb99c60443fdbb368119
Rawan <rawansafouan@gmail.com>
2026-04-26 21:22:09 +0200

    rectified with betty

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,51 +2,70 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
- * @head: double pointer to head of list
- * @index: index of node to delete (starting at 0)
+ * delete_head - deletes the head node
+ * @head: double pointer to list head
  *
- * Return: 1 if success, -1 if failure
+ * Return: 1 on success
+ */
+static int delete_head(dlistint_t **head)
+{
+	dlistint_t *tmp;
+
+	tmp = *head;
+	*head = tmp->next;
+
+	if (*head != NULL)
+		(*head)->prev = NULL;
+
+	free(tmp);
+	return (1);
+}
+
+/**
+ * delete_node - deletes a middle or last node
+ * @node: node to delete
+ *
+ * Return: 1 on success
+ */
+static int delete_node(dlistint_t *node)
+{
+	if (node->prev != NULL)
+		node->prev->next = node->next;
+
+	if (node->next != NULL)
+		node->next->prev = node->prev;
+
+	free(node);
+	return (1);
+}
+
+/**
+ * delete_dnodeint_at_index - deletes node at index
+ * @head: double pointer to head
+ * @index: index of node to delete
+ *
+ * Return: 1 if success, -1 if fail
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp;
-	dlistint_t *to_delete;
-	unsigned int i = 0;
+	dlistint_t *tmp;
+	unsigned int i;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
 
-	temp = *head;
-
 	if (index == 0)
-	{
-		*head = temp->next;
+		return (delete_head(head));
 
-		if (*head != NULL)
-			(*head)->prev = NULL;
+	tmp = *head;
+	i = 0;
 
-		free(temp);
-		return (1);
-	}
-
-	while (temp != NULL)
+	while (tmp != NULL)
 	{
 		if (i == index)
-		{
-			to_delete = temp;
+			return (delete_node(tmp));
 
-			if (temp->prev != NULL)
-				temp->prev->next = temp->next;
-
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-
-			free(to_delete);
-			return (1);
-		}
-
-		temp = temp->next;
+		tmp = tmp->next;
 		i++;
 	}
 
