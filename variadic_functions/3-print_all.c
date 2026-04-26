f60fa0a84ef804f8bb9cfb1f627163ea900ed373
Rawan <rawansafouan@gmail.com>
2026-04-26 20:38:04 +0200

    rectified betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,33 +3,36 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything based on format
- * @format: list of types
- *
- * Return: void
+ * print_c - prints char
+ * @args: argument list
  */
-
-typedef struct print_op
-{
-	char *op;
-	void (*f)(va_list *);
-} print_op_t;
-
 void print_c(va_list *args)
 {
 	printf("%c", va_arg(*args, int));
 }
 
+/**
+ * print_i - prints integer
+ * @args: argument list
+ */
 void print_i(va_list *args)
 {
 	printf("%d", va_arg(*args, int));
 }
 
+/**
+ * print_f - prints float
+ * @args: argument list
+ */
 void print_f(va_list *args)
 {
 	printf("%f", va_arg(*args, double));
 }
 
+/**
+ * print_s - prints string
+ * @args: argument list
+ */
 void print_s(va_list *args)
 {
 	char *s = va_arg(*args, char *);
@@ -39,6 +42,12 @@ void print_s(va_list *args)
 	printf("%s", s);
 }
 
+/**
+ * print_all - prints anything based on format
+ * @format: format string
+ *
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
 	unsigned int i = 0, j;
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,11 +1,23 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-/* prototypes here */
+#include <stdarg.h>
+
+/**
+ * struct print_op - matches format with function
+ * @op: format character
+ * @f: function pointer
+ */
+typedef struct print_op
+{
+	char *op;
+	void (*f)(va_list *);
+} print_op_t;
+
+/* prototypes */
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 
 #endif
