29d96a3a164cad2c7610f3ee5fecc02583973bca
Rawan <rawansafouan@gmail.com>
2026-04-26 18:50:22 +0200

    adding files task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
+ * @a: pointer to the array
+ * @n: number of elements to print
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+			printf(", ");
+		printf("%d", a[i]);
+	}
+
+	printf("\n");
+}
