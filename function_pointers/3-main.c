5cbeb66e737b88040048ecaa51bd4bf0df04101f
Rawan <rawansafouan@gmail.com>
2026-04-26 20:16:44 +0200

    adding file task 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 or error codes
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	f = get_op_func(argv[2]);
+
+	if (f == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	printf("%d\n", f(a, b));
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
