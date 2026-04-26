523d389c695e67b6b14091faaf9e7b74cc7da500
Rawan <rawansafouan@gmail.com>
2026-04-26 18:43:22 +0200

    adding files task 5 and updates

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int len = 0;
+	char temp;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len--;
+
+	while (i < len)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+		i++;
+		len--;
+	}
+}
