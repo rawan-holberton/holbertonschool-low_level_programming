8f1e8365544f181b3c637a83f6a62c801cd41d44
Rawan <rawansafouan@gmail.com>
2026-04-26 18:47:22 +0200

    rectified base on the checker

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string,
- * starting with the first character, followed by a new line
- * @str: pointer to the string
+ * puts2 - prints every other character of a string
+ * @str: input string
  *
  * Return: void
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 
 	_putchar('\n');
